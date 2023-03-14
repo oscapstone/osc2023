b046cb49f0b3b043cde1d9026288eda6e8955a0c
RobertttBS <k0983357798@gmail.com>
2023-03-14 17:41:39 +0800

    Get the dtb_address and complete `swap_endian()`

--- src/dtb.c
+++ src/dtb.c
@@ -7,55 +7,24 @@ extern unsigned char __dtb_address;
 
 fdt_header *dtb_address;
 
+uint32_t swap_endian(uint32_t num)
+{
+    return num >> 24 | num << 24 | (num & 0xff00) << 8 | (num & 0xff0000) >> 8;
+}
+
+
 void fdt_init()
 {
-    // dtb_address = (fdt_header *) &__dtb_address;
     uint32_t *tmp_pointer = (uint32_t *) &__dtb_address;
     // uint32_t *tmp_pointer = (uint32_t *) 0x7fff0;
 
-    uart_puts("tmp_pointer :");
-    uart_hex(tmp_pointer);
-    uart_send('\n');
-
     dtb_address = (fdt_header *) *tmp_pointer;
-
-    uart_puts("dtb_address :");
-    uart_hex(dtb_address);
-    uart_send('\n');
-    uart_puts("total size :");
-    uart_hex(dtb_address->totalsize);
-    uart_send('\n');
-
-    // readfile(dtb_address, dtb_address->totalsize);
-    uart_puts("\nafter this\n");
 }
 
 
 void fdt_traverse()
 {
-
-    uart_puts("dtb_address :");
-    uart_hex(dtb_address);
-    uart_send('\n');
-
-    // take a look
-    uart_puts("magic :");
-    uart_hex(dtb_address->magic);
-    uart_send('\n');
-
-    uart_puts("total size :");
-    uart_hex((int) dtb_address->totalsize);
-    uart_send('\n');
-
-    uart_puts("right magic :");
-    uart_hex(FTB_MAGIC);
-    uart_send('\n');
-
-    
-
-
-    // readfile((char *) dtb_address, sizeof(fdt_header));
-    // uart_send('\n');
+    uart_hex(swap_endian(dtb_address->magic));
 }
 
 
