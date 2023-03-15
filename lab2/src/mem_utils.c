e65082d339119f8f611ea4da007f6de9c4387ea9
sheauni <nini1020810@gmail.com>
2023-03-15 16:33:39 +0800

    lab2: adds max heap size

--- lab2/src/mem_utils.c
+++ lab2/src/mem_utils.c
@@ -1,5 +1,7 @@
 #include "mini_uart.h"
 
+#define HEAP_SIZE 0x10000
+
 extern char heap_start;
 
 static char *heap_tail = &heap_start;
@@ -7,6 +9,10 @@ static int heap_current_size = 0;
 
 void* simple_malloc(unsigned int size)
 {
+        if (heap_current_size + size > HEAP_SIZE) {
+                uart_send_string("[ERROR] exceed max heap size\r\n");
+                return (char*) 0;
+        }
         char* adr = heap_tail;
         heap_current_size += size;
         heap_tail += size;
