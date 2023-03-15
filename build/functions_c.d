34ccbcb8657bdb7ca5df2fcd124042ab6e1d3a7c
yzhuang <Nuh>
2023-03-16 03:41:44 +0800

    remove ignored files

--- build/functions_c.d
+++
@@ -1,2 +0,0 @@
-build/functions_c.o: src/functions.c include/mini_uart.h include/mbox.h \
- include/utils.h
--- build/mbox_c.d
+++
@@ -1,2 +0,0 @@
-build/mbox_c.o: src/mbox.c include/mini_uart.h include/peripherals/mbox.h \
- include/peripherals/base.h
