94f4328b28d8745644d9dc8218ce91cb770e3f0f
nigauri <suyin.hsu.tw@gmail.com>
2023-03-22 16:46:08 +0800

    update

--- lab2/include/mem.h
+++
@@ -1,8 +0,0 @@
-#ifndef MEM_H
-#define MEM_H
-
-#include <stdint.h>
-
-void* simple_malloc(uint32_t size);
-
-#endif
--- lab2/src/kernel.c
+++
@@ -1,8 +0,0 @@
-#include "mini_uart.h"
-#include "shell.h"
-
-void kernel_start(void){
-    uart_init();
-
-    simple_shell();
-}
