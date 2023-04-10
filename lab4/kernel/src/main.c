6a464bc8f807c861d637e9182926d1d87bbff1a2
Yin-Tao Ling <ms0599027@gmail.com>
2023-04-10 12:58:15 +0800

    [LAB4] Modification for RPI Demo on Windows OS

--- lab4/kernel/src/main.c
+++ lab4/kernel/src/main.c
@@ -14,8 +14,6 @@ void main(char* arg){
     dtb_ptr = arg;
     traverse_device_tree(dtb_ptr, dtb_callback_initramfs); // get initramfs location from dtb
 
-    init_allocator();
-
     uart_init();
     irqtask_list_init();
     timer_list_init();
@@ -23,6 +21,10 @@ void main(char* arg){
     uart_interrupt_enable();
     el1_interrupt_enable();  // enable interrupt in EL1 -> EL1
     core_timer_enable();
+
+    cli_cmd_read(input_buffer); // Wait for input, Windows cannot attach to SERIAL from two processes.
+
+    init_allocator();
 
     cli_print_banner();
     while(1){
