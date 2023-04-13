a5efcc4468011e82f1fe02758cdaa3c9ac83f639
Ginny Chien <ginny1010377@gmail.com>
2023-04-13 18:34:11 +0800

    PR

--- Lab3/kernel/include/uart.h
+++ Lab3/kernel/include/uart.h
@@ -30,7 +30,7 @@ int uart_printf(char *fmt, ...);
 void uart_puts(char *s);
 void uart_hex(unsigned int d);
 char uart_get();
-
+ 
 void init_uart_async_index();
 void enable_uart_interrupt();
 void enable_uart_tx_interrupt();
