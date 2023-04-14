d59f7206aaec484e79798f38563a8da094bcf32c
Yin-Tao Ling <ms0599027@gmail.com>
2023-04-14 11:12:24 +0800

    [LAB5] Initial Commit

---
+++ lab5/kernel/include/shell.h
@@ -0,0 +1,32 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#define CMD_MAX_LEN 0x100
+#define MSG_MAX_LEN 0x100
+
+typedef struct CLI_CMDS
+{
+    char command[CMD_MAX_LEN];
+    char help[MSG_MAX_LEN];
+} CLI_CMDS;
+
+int  cli_cmd_strcmp(const char*, const char*);
+void cli_cmd_clear(char*, int);
+void cli_cmd_read(char*);
+void cli_cmd_exec(char*);
+void cli_print_banner();
+
+void do_cmd_cat(char*);
+void do_cmd_dtb();
+void do_cmd_exec(char*);
+void do_cmd_help();
+void do_cmd_hello();
+void do_cmd_info();
+void do_cmd_s_allocator();
+void do_cmd_ls(char*);
+void do_cmd_memory_tester();
+void do_cmd_setTimeout(char* msg, char* sec);
+void do_cmd_set2sAlert();
+void do_cmd_reboot();
+
+#endif /* _SHELL_H_ */
