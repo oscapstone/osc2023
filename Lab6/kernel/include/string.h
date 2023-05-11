1da277ba3e02d590414e22ceb598c3271bdc021f
Richard <yisin11012@gmail.com>
2023-05-11 11:16:09 +0800

    update Lab6 - vitual memory in kernel space, vitual memory in user space, vedio player, mmap, page fault handler & demand paging

---
+++ Lab6/kernel/include/string.h
@@ -0,0 +1,21 @@
+#ifndef _STRING_H_
+#define _STRING_H_
+
+#include "stddef.h"
+#define VSPRINT_MAX_BUF_SIZE 0x100
+
+unsigned int sprintf(char *dst, char* fmt, ...);
+unsigned int vsprintf(char *dst,char* fmt, __builtin_va_list args);
+
+unsigned long long strlen(const char *str);
+int                strcmp(const char*, const char*);
+int                strncmp(const char*, const char*, unsigned long long);
+char*              memcpy(void *dest, const void *src, unsigned long long len);
+char*              strcpy(char *dest, const char *src);
+void*              memset(void *s, int c, size_t n);
+char*              strchr(register const char *s, int c);
+
+char* str_SepbySpace(char* head);
+int   atoi(char* str);
+
+#endif /* _STRING_H_ */
