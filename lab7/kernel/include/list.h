f5be457a4a04de7c11751bd81e149ed0e72356de
Yin-Tao Ling <ms0599027@gmail.com>
2023-05-12 17:50:55 +0800

    [LAB7] Initial commit

---
+++ lab7/kernel/include/list.h
@@ -0,0 +1,137 @@
+#ifndef LIST_H
+#define LIST_H
+
+/*
+ * Circular doubly linked list implementation.
+ *
+ * Some of the internal functions ("__xxx") are useful when
+ * manipulating whole lists rather than single entries, as
+ * sometimes we already know the next/prev entries and we can
+ * generate better code by using them directly rather than
+ * using the generic single-entry routines.
+ *
+ * https://github.com/torvalds/linux/blob/master/include/linux/list.h
+ * https://elixir.bootlin.com/linux/latest/source/scripts/kconfig/list.h#L24
+ */
+
+typedef struct list_head {
+	struct list_head *next, *prev;
+}list_head_t;
+
+#define LIST_HEAD_INIT(name) { &(name), &(name) }
+
+#define LIST_HEAD(name) \
+	struct list_head name = LIST_HEAD_INIT(name)
+
+/**
+ * INIT_LIST_HEAD - Initialize a list_head structure
+ * @list: list_head structure to be initialized.
+ *
+ * Initializes the list_head to point to itself.  If it is a list header,
+ * the result is an empty list.
+ */
+static inline void INIT_LIST_HEAD(struct list_head *list)
+{
+	list->next = list;
+	list->prev = list;
+}
+
+static inline void __list_add(struct list_head *new,
+			      struct list_head *prev,
+			      struct list_head *next)
+{
+	next->prev = new;
+	new->next = next;
+	new->prev = prev;
+	prev->next = new;
+}
+
+/**
+ * list_add - add a new entry
+ * @new: new entry to be added
+ * @head: list head to add it after
+ *
+ * Insert a new entry after the specified head.
+ * This is good for implementing stacks.
+ */
+static inline void list_add(struct list_head *new, struct list_head *head)
+{
+	__list_add(new, head, head->next);
+}
+
+/**
+ * list_add_tail - add a new entry
+ * @new: new entry to be added
+ * @head: list head to add it before
+ *
+ * Insert a new entry before the specified head.
+ * This is useful for implementing queues.
+ */
+static inline void list_add_tail(struct list_head *new, struct list_head *head)
+{
+	__list_add(new, head->prev, head);
+}
+
+/*
+ * Delete a list entry by making the prev/next entries
+ * point to each other.
+ *
+ * This is only for internal list manipulation where we know
+ * the prev/next entries already!
+ */
+static inline void __list_del(struct list_head * prev, struct list_head * next)
+{
+	next->prev = prev;
+	prev->next = next;
+}
+
+static inline void list_del_entry(struct list_head *entry)
+{
+	__list_del(entry->prev, entry->next);
+}
+
+
+/**
+ * list_is_head - tests whether @list is the list @head
+ * @list: the entry to test
+ * @head: the head of the list
+ */
+static inline int list_is_head(const struct list_head *list, const struct list_head *head)
+{
+	return list == head;
+}
+
+
+/**
+ * list_empty - tests whether a list is empty
+ * @head: the list to test.
+ */
+static inline int list_empty(const struct list_head *head)
+{
+	return head->next == head;
+}
+
+/**
+ * list_for_each	-	iterate over a list
+ * @pos:	the &struct list_head to use as a loop cursor.
+ * @head:	the head for your list.
+ */
+#define list_for_each(pos, head) \
+	for (pos = (head)->next; !list_is_head(pos, (head)); pos = pos->next)
+
+/**
+ * list_empty - tests whether a list is empty
+ * @head: the list to test.
+ */
+static inline int list_size(const struct list_head *head)
+{
+	list_head_t *pos;
+	int i= 0;
+	list_for_each(pos, head)
+	{
+		i++;
+	}
+	return i;
+}
+
+#endif
---
+++ lab7/kernel/src/syscall.c
@@ -0,0 +1,260 @@
+#include "syscall.h"
+#include "cpio.h"
+#include "sched.h"
+#include "stddef.h"
+#include "uart1.h"
+#include "exception.h"
+#include "memory.h"
+#include "mbox.h"
+#include "signal.h"
+#include "mmu.h"
+#include "string.h"
+
+int getpid(trapframe_t* tpf)
+{
+    tpf->x0 = curr_thread->pid;
+    return curr_thread->pid;
+}
+
+size_t uartread(trapframe_t *tpf,char buf[], size_t size)
+{
+    int i = 0;
+    for (int i = 0; i < size;i++)
+    {
+        buf[i] = uart_async_getc();
+    }
+    tpf->x0 = i;
+    return i;
+}
+
+size_t uartwrite(trapframe_t *tpf,const char buf[], size_t size)
+{
+    int i = 0;
+    for (int i = 0; i < size; i++)
+    {
+        uart_async_putc(buf[i]);
+    }
+    tpf->x0 = i;
+    return i;
+}
+
+//In this lab, you won’t have to deal with argument passing
+int exec(trapframe_t *tpf,const char *name, char *const argv[])
+{
+    mmu_del_vma(curr_thread);
+    INIT_LIST_HEAD(&curr_thread->vma_list);
+
+    curr_thread->datasize = get_file_size((char *)name);
+    char *new_data = get_file_start((char *)name);
+    curr_thread->data = kmalloc(curr_thread->datasize);
+    curr_thread->stack_alloced_ptr = kmalloc(USTACK_SIZE);
+
+    asm("dsb ish\n\t");      // ensure write has completed
+    mmu_free_page_tables(curr_thread->context.pgd, 0);
+    memset(PHYS_TO_VIRT(curr_thread->context.pgd), 0, 0x1000);
+    asm("tlbi vmalle1is\n\t" // invalidate all TLB entries
+        "dsb ish\n\t"        // ensure completion of TLB invalidatation
+        "isb\n\t");          // clear pipeline
+
+    mmu_add_vma(curr_thread,              USER_KERNEL_BASE,             curr_thread->datasize, (size_t)VIRT_TO_PHYS(curr_thread->data)             , 0b111, 1);
+    mmu_add_vma(curr_thread, USER_STACK_BASE - USTACK_SIZE,                       USTACK_SIZE, (size_t)VIRT_TO_PHYS(curr_thread->stack_alloced_ptr), 0b111, 1);
+    mmu_add_vma(curr_thread,              PERIPHERAL_START, PERIPHERAL_END - PERIPHERAL_START,                                     PERIPHERAL_START, 0b011, 0);
+    mmu_add_vma(curr_thread,        USER_SIGNAL_WRAPPER_VA,                            0x2000,         (size_t)VIRT_TO_PHYS(signal_handler_wrapper), 0b101, 0);
+
+    memcpy(curr_thread->data, new_data, curr_thread->datasize);
+    for (int i = 0; i <= SIGNAL_MAX; i++)
+    {
+        curr_thread->signal_handler[i] = signal_default_handler;
+    }
+
+
+    tpf->elr_el1 = USER_KERNEL_BASE;
+    tpf->sp_el0 = USER_STACK_BASE;
+    tpf->x0 = 0;
+    return 0;
+}
+
+int fork(trapframe_t *tpf)
+{
+    lock();
+    thread_t *newt = thread_create(curr_thread->data,curr_thread->datasize);
+
+    //copy signal handler
+    for (int i = 0; i <= SIGNAL_MAX;i++)
+    {
+        newt->signal_handler[i] = curr_thread->signal_handler[i];
+    }
+
+    list_head_t *pos;
+    vm_area_struct_t *vma;
+    list_for_each(pos, &curr_thread->vma_list){
+        // ignore device and signal wrapper
+        vma = (vm_area_struct_t *)pos;
+        if (vma->virt_addr == USER_SIGNAL_WRAPPER_VA || vma->virt_addr == PERIPHERAL_START)
+        {
+            continue;
+        }
+        char *new_alloc = kmalloc(vma->area_size);
+        mmu_add_vma(newt, vma->virt_addr, vma->area_size, (size_t)VIRT_TO_PHYS(new_alloc), vma->rwx, 1);
+        memcpy(new_alloc, (void*)PHYS_TO_VIRT(vma->phys_addr), vma->area_size);
+    }
+    mmu_add_vma(newt,       PERIPHERAL_START, PERIPHERAL_END - PERIPHERAL_START,                             PERIPHERAL_START, 0b011, 0);
+    mmu_add_vma(newt, USER_SIGNAL_WRAPPER_VA,                            0x2000, (size_t)VIRT_TO_PHYS(signal_handler_wrapper), 0b101, 0);
+
+    int parent_pid = curr_thread->pid;
+
+    //copy stack into new process
+    for (int i = 0; i < KSTACK_SIZE; i++)
+    {
+        newt->kernel_stack_alloced_ptr[i] = curr_thread->kernel_stack_alloced_ptr[i];
+    }
+
+    store_context(get_current());
+    //for child
+    if( parent_pid != curr_thread->pid)
+    {
+        goto child;
+    }
+
+    void *temp_pgd = newt->context.pgd;
+    newt->context = curr_thread->context;
+    newt->context.pgd = VIRT_TO_PHYS(temp_pgd);
+    newt->context.fp += newt->kernel_stack_alloced_ptr - curr_thread->kernel_stack_alloced_ptr; // move fp
+    newt->context.sp += newt->kernel_stack_alloced_ptr - curr_thread->kernel_stack_alloced_ptr; // move kernel sp
+
+    unlock();
+
+    tpf->x0 = newt->pid;
+    return newt->pid;
+
+child:
+    tpf->x0 = 0;
+    return 0;
+}
+
+void exit(trapframe_t *tpf, int status)
+{
+    thread_exit();
+}
+
+int syscall_mbox_call(trapframe_t *tpf, unsigned char ch, unsigned int *mbox_user)
+{
+    lock();
+
+    unsigned int size_of_mbox = mbox_user[0];
+    memcpy((char *)pt, mbox_user, size_of_mbox);
+    mbox_call(MBOX_TAGS_ARM_TO_VC, (unsigned int)((unsigned long)&pt));
+    memcpy(mbox_user, (char *)pt, size_of_mbox);
+
+    tpf->x0 = 8;
+    unlock();
+    return 0;
+}
+
+void kill(trapframe_t *tpf,int pid)
+{
+    lock();
+    if (pid >= PIDMAX || pid < 0  || !threads[pid].isused)
+    {
+        unlock();
+        return;
+    }
+    threads[pid].iszombie = 1;
+    unlock();
+    schedule();
+}
+
+void signal_register(int signal, void (*handler)())
+{
+    if (signal > SIGNAL_MAX || signal < 0)return;
+
+    curr_thread->signal_handler[signal] = handler;
+}
+
+void signal_kill(int pid, int signal)
+{
+    if (pid > PIDMAX || pid < 0 || !threads[pid].isused)return;
+
+    lock();
+    threads[pid].sigcount[signal]++;
+    unlock();
+}
+
+//only need to implement the anonymous page mapping in this Lab.
+void *mmap(trapframe_t *tpf, void *addr, size_t len, int prot, int flags, int fd, int file_offset)
+{
+    // Ignore flags as we have demand pages
+
+    // Req #3 Page size round up
+    len = len % 0x1000 ? len + (0x1000 - len % 0x1000) : len;
+    addr = (unsigned long)addr % 0x1000 ? addr + (0x1000 - (unsigned long)addr % 0x1000) : addr;
+
+    // Req #2 check if overlap
+    list_head_t *pos;
+    vm_area_struct_t *vma;
+    vm_area_struct_t *the_area_ptr = 0;
+    list_for_each(pos, &curr_thread->vma_list)
+    {
+        vma = (vm_area_struct_t *)pos;
+        // Detect existing vma overlapped
+        if ( ! (vma->virt_addr >= (unsigned long)(addr + len) || vma->virt_addr + vma->area_size <= (unsigned long)addr ) )
+        {
+            the_area_ptr = vma;
+            break;
+        }
+    }
+    // take as a hint to decide new region's start address
+    if (the_area_ptr)
+    {
+        tpf->x0 = (unsigned long) mmap(tpf, (void *)(the_area_ptr->virt_addr + the_area_ptr->area_size), len, prot, flags, fd, file_offset);
+        return (void *)tpf->x0;
+    }
+    // create new valid region, map and set the page attributes (prot)
+    mmu_add_vma(curr_thread, (unsigned long)addr, len, VIRT_TO_PHYS((unsigned long)kmalloc(len)), prot, 1);
+    tpf->x0 = (unsigned long)addr;
+    return (void*)tpf->x0;
+}
+
+void sigreturn(trapframe_t *tpf)
+{
+    //unsigned long signal_ustack = tpf->sp_el0 % USTACK_SIZE == 0 ? tpf->sp_el0 - USTACK_SIZE : tpf->sp_el0 & (~(USTACK_SIZE - 1));
+    //kfree((char*)signal_ustack);
+    load_context(&curr_thread->signal_saved_context);
+}
+
+
+
+
+char* get_file_start(char *thefilepath)
+{
+    char *filepath;
+    char *filedata;
+    unsigned int filesize;
+    struct cpio_newc_header *header_pointer = CPIO_DEFAULT_START;
+
+    while (header_pointer != 0)
+    {
+        int error = cpio_newc_parse_header(header_pointer, &filepath, &filesize, &filedata, &header_pointer);
+        if (error) break;
+        if (strcmp(thefilepath, filepath) == 0) return filedata;
+        if (header_pointer == 0) uart_puts("execfile: %s: No such file or directory\r\n", thefilepath);
+    }
+    return 0;
+}
+
+unsigned int get_file_size(char *thefilepath)
+{
+    char *filepath;
+    char *filedata;
+    unsigned int filesize;
+    struct cpio_newc_header *header_pointer = CPIO_DEFAULT_START;
+
+    while (header_pointer != 0)
+    {
+        int error = cpio_newc_parse_header(header_pointer, &filepath, &filesize, &filedata, &header_pointer);
+        if (error) break;
+        if (strcmp(thefilepath, filepath) == 0) return filesize;
+        if (header_pointer == 0) uart_puts("execfile: %s: No such file or directory\r\n", thefilepath);
+    }
+    return 0;
+}
